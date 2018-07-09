c4cb2ebb5857a9e2e0e3524c3b7ff7a9584b78b6
David Saint <topstronics@gmail.com>
2018-07-09 11:30:05 +0100

    Finished basic development of the homepage

---
+++ src/index.js
@@ -0,0 +1,5 @@
+require('./styles/app.scss');
+
+require('materialize-css');
+
+require('./js/main');
---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+const navOptions = {
+  edge: 'right'
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+  let sidenav = document.querySelectorAll('.sidenav');
+  let navInstances = M.Sidenav.init(sidenav, navOptions);
+
+  let collapsible = document.querySelectorAll('.collapsible');
+  let collInstances = M.Collapsible.init(collapsible, { });
+
+  let expandable = document.querySelectorAll('.collapsible.expandable');
+  let expInstances = M.Collapsible.init(expandable, { accordion: false });
+
+  let tabs = document.querySelectorAll('.tabs');
+  var tabsInstance = M.Tabs.init(tabs, {});
+});
